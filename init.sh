b9f824d639f9099b7aa4da0f89fec178c6f1ab87
barath ramalingam <benspylee@hotmail.com>
2023-05-31 18:30:55 +0530

    script update

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@ mkdir -p data/grafana_data
 mkdir -p data/tempo_data
 mkdir -p data/prometheus_data
 mkdir -p data/alertmanager_data
+mkdir -p data/loki_data
 
+sudo chmod -R 777  data/
 
-mkdir -p config/grafana
-mkdir -p config/tempo
-mkdir -p config/prometheus
-mkdir -p config/alertmanager
+#mkdir -p config/grafana
+#mkdir -p config/tempo
+#mkdir -p config/prometheus
+#mkdir -p config/alertmanager
